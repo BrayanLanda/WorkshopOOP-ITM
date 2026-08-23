8a75f8afffae8b11251445d8dcf0b14aae6ff88b
Brayan Landa <landakkpp@gmail.com>
2026-08-23 18:21:15 -0500

    feat: Implement Time class with constructors and validation methods

--- Backend/Time.cs
+++ Backend/Time.cs
@@ -2,5 +2,42 @@
 
 public class Time
 {
+    //Fields
+    private int _hour;
+    private int _millisecond;
+    private int _minute;
+    private int _second;
 
+    //Constructors
+    public Time(){}
+    public Time(int hour)
+    {
+        _hour = hour;
+    }
+    public Time(int hour, int minute)
+    {
+        _hour = hour;
+        _minute = minute;
+    }
+    public Time(int hour, int minute, int second)
+    {
+        _hour = hour;
+        _minute = minute;
+        _second = second;
+    }
+    public Time(int hour, int minute, int secound, int millisecond)
+    {
+        _hour = hour;
+        _minute = minute;
+        _secoud = secound;
+        _millisecond = millisecond;
+    }
+
+    //Methods
+
+    //Private validations
+    private bool ValidHour(int hour) => hour >= 0 && hour <= 23;
+    private bool ValidMinute(int minute) => minute >= 0 && minute <= 59;
+    private bool ValidSecond(int second) => second >= 0 && second <= 29;
+    private bool ValidMillisecond(int millisecond) => millisecond >= 0 && millisecond <= 999;
 }
