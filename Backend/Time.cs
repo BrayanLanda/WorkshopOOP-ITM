d9ca26a7be863fd4ecc1f7d49565ac5c6a694a3e
Brayan Landa <landakkpp@gmail.com>
2026-08-23 19:33:22 -0500

    feat: Enhance Time class with validation properties and fix constructor parameter typo; add Frontend project files

--- Backend/Time.cs
+++ Backend/Time.cs
@@ -9,7 +9,7 @@ public class Time
     private int _second;
 
     //Constructors
-    public Time(){}
+    public Time() { }
     public Time(int hour)
     {
         _hour = hour;
@@ -25,14 +25,58 @@ public class Time
         _minute = minute;
         _second = second;
     }
-    public Time(int hour, int minute, int secound, int millisecond)
+    public Time(int hour, int minute, int second, int millisecond)
     {
         _hour = hour;
         _minute = minute;
-        _secoud = secound;
+        _second = second;
         _millisecond = millisecond;
     }
 
+    //Properties
+    public int Hour
+    {
+        get => _hour;
+        set
+        {
+            if (!ValidHour(value))
+                throw new ArgumentException($"The hour: {value}, is not valid.");
+            _hour = value;
+        }
+    }
+
+    public int Minute
+    {
+        get => _minute;
+        set
+        {
+            if (!ValidMinute(value))
+                throw new ArgumentException($"The minute: {value}, is not valid.");
+            _minute = value;
+        }
+    }
+
+    public int Second
+    {
+        get => _second;
+        set
+        {
+            if (!ValidSecond(value))
+                throw new ArgumentException($"The second: {value}, is not valid.");
+            _second = value;
+        }
+    }
+
+    public int Millisecond
+    {
+        get => _millisecond;
+        set
+        {
+            if (!ValidMillisecond(value))
+                throw new ArgumentException($"The millisecond: {value}, is not valid.");
+            _millisecond = value;
+        }
+    }
     //Methods
 
     //Private validations
